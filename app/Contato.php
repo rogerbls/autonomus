a2019bb79645c14e85438eb18ccb1e191558e69e
Roger <rogerbls51@gmail.com>
2019-09-26 18:46:01 -0300

    classe contato

--- app/Contato.php
+++ app/Contato.php
@@ -9,4 +9,29 @@ class Contato extends Model{
     protected $fillable = [
         'saudacao' , 'nome', 'telefone', 'email', 'data_nascimento', 'avatar', 'observacao'
     ];
+
+    //Acessar
+    public function getAvatarImageAttribute($value){
+        return $this->avatar == null ? asset('images/null.png'): asset($this->avatar);
+    }
+    public function getAvatarFilenameAttribute($value){
+        return substr($this->avatar, 30, strlen($this->avatar));
+    }
+    public function getDataNascimentoAttribute($value){
+        return dateFormatDatabaseScreen($value, 'screen');
+    }
+
+    //Mutator
+    public function setDataNascimentoAttribute($value){
+        $this->attributes['data_nascimento'] = dateFormatDatabaseScreen($value);
+    }
+    public function setAvatarAtribute($value){
+        $filename = substr(md5(rand(100000,999999)),0,5) .'_'.$value->getClientOriginalFileName();
+        $filepath = 'public/uploads/'.date('Y').'/'.date('m').'/';
+        if($value->isValid()){
+            $path= $value->storeAs($filepath, $filename);
+        }
+        $this->attributes['avatar'] = str_replace('public', 'storage', $filepath) . $filename;
+    }
+
 }
