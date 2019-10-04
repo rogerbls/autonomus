bb69161ee4cee7cd520276dd1d21d6c91a6a27de
Roger <rogerbls51@gmail.com>
2019-10-04 13:07:21 -0300

    Ajustes dinais contatos

--- app/Http/Requests/ContatoRequest.php
+++ app/Http/Requests/ContatoRequest.php
@@ -11,7 +11,8 @@ class ContatoRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()  {
+    public function authorize()
+    {
         return true;
     }
 
@@ -20,39 +21,41 @@ class ContatoRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()    {
-        switch($this->method()){
-            case 'POST':
+    public function rules()
+    {
+        switch($this->method()) {
+            case "POST": // CRIAÇÃO DE UM NOVO REGISTRO
                 return [
                     'saudacao' => 'required|max:5',
-                    'nome'=>'required|max:100',
-                    'telefone'=> 'required|max:15',
-                    'email'=>'email|max:200|unique:contatos',
-                    'data_nascimento'=>'date_format:"d/m/Y"',
-                    'avatar'=>'nullable|sometimes|image|mimes:jpg,jpeg,png,gif'
-                ];
-            break;
-
-            case 'PUT':
-                return [
-                    'saudacao' => 'required|max:5',
-                    'nome'=>'required|max:100',
-                    'telefone'=> 'required|max:15',
-                    'email'=>'email|max:200|unique:contatos,email,'.$this->id,
-                    'data_nascimento'=>'date_format:"d/m/Y"',
-                    'avatar'=>'nullable|sometimes|image|mimes:jpg,jpeg,png,gif'
+                    'nome' => 'required|max:100',
+                    'telefone' => 'required|max:15',
+                    'email' => 'email|max:200|unique:contatos',
+                    'data_nascimento' => 'date_format:"d/m/Y"',
+                    'avatar' => 'nullable|sometimes|image|mimes:jpg,jpeg,png,gif'
                 ];
                 break;
 
+            case "PUT": // ATUALIZAÇÃO DE UM REGISTRO EXISTENTE
+                return [
+                    'saudacao' => 'required|max:5',
+                    'nome' => 'required|max:100',
+                    'telefone' => 'required|max:15',
+                    'email' => 'email|max:200|unique:contatos,email,'.$this->id,
+                    'data_nascimento' => 'date_format:"d/m/Y"',
+                    'avatar' => 'nullable|sometimes|image|mimes:jpg,jpeg,png,gif'
+                ];
+                break;
             default:break;
         }
     }
-    public function messages(){
-        return[
-            'saudacao.required' => 'O campo saudação é obrigatório',
-            'nome.required' => 'O campo nome é obrigatório',
-            'email.email'=>'Informe um email válido',
-            'data_nascimento.required'=>'O campo data deve ser no formato DD/MM/AAAA'
+
+    public function messages()
+    {
+        return [
+            'saudacao.required' => 'O campo Saudação é obrigatório',
+            'nome.required' => 'O campo Nome é obrigatório',
+            'email.email' => 'Informe um e-mail válido',
+            'data_nascimento.date_format' => 'O campo data deve ser no formato DD/MM/AAAA'
         ];
     }
 }
